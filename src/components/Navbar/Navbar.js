ce112b71ab6b0a0d533bcf41ed26e7747bb22711
Marc <marcmartinez2001@gmail.com>
2024-03-06 15:55:18 +0100

    Updating suiting images and styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
-import logo from '../../logoCentro.PNG';
-import Search from '../card/search';
+import logo from '../../PokemonLogo.png';
+// import Search from '../card/search';
 
 const NavbarMenu = () => {
     return (
         <div className="Menu">
             <Navbar bg="dark" variant="dark" expand="lg">
                 <Container fluid>
-                    <Navbar.Brand href="#/"><img src={logo} width="30" height="30" alt="logo" /></Navbar.Brand>
+                    <Navbar.Brand href="#/"><img src={logo} width="150" height="50" alt="logo" /></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ReactDOM from 'react-dom';
 

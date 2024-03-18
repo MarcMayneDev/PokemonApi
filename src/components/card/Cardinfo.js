17eb7a97c419dfd56d96469998c95e7604c3bf49
Marc <marcmartinez2001@gmail.com>
2024-03-18 22:19:50 +0100

    feat: migrate CardInfo to Material UI and install react types

--- src/components/card/Cardinfo.js
+++ src/components/card/Cardinfo.js
@@ -1,51 +1,44 @@
-import "./Cardinfo.css";
-
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  Link,
+  Typography,
+} from "@mui/material";
 import React from "react";
+
+import TypePill from "../TypePill";
 
 export const Cardinfo = ({ info }) => {
   const { id, sprites, name, types, height, weight } = info;
 
   return (
-    <div>
-      <div className="card" style={{ width: "14rem" }}>
-        <div className="card-header">
-          <h5 className="card-title">{id}</h5>
-        </div>
-        <div className="card-body">
-          <img
-            src={sprites.front_default}
-            alt={name}
-            className="card-title"
-            loading="lazy"
-            fetchpriority={id < 15 ? "high" : "low"}
-          />
-        </div>
-        <div className="card-footer">
-          <p className="card-text">{name}</p>
-          <p className="card-text">Altura: {height / 10}m</p>
-          <p className="card-text">Peso: {weight / 10}Kg</p>
-          <div style={{ display: "flex", flexDirection: "row" }}>
-            <p className="card-text">Tipos:</p>
-            {types.map((p) => (
-              <div
-                className={p.type.name}
-                style={{
-                  backgroundColor: "grey",
-                  color: "white",
-                  width: "4rem",
-                  textAlign: "center",
-                  flex: "1",
-                  margin: "4px",
-                }}
-                key={p.type.name}
-              >
-                {p.type.name}
-              </div>
+    <Card sx={{ width: "16rem" }}>
+      <img src={sprites.front_default} alt={name} className="card-title" />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {`N.º ${String(id).padStart(4, "0")}`}
+        </Typography>
+        <Typography gutterBottom variant="body1">
+          {`${name.charAt(0).toUpperCase()}${name.slice(1)}`}
+        </Typography>
+        <Typography gutterBottom variant="body1">
+          {height / 10}m
+        </Typography>
+        <Typography gutterBottom variant="body1">
+          {weight / 10}Kg
+        </Typography>
+        <Box display="flex" gap="8px">
+          {types &&
+            types.map(({ type }) => (
+              <TypePill type={type.name} key={type.name} />
             ))}
-          </div>
-        </div>
-        <a href="?#/pokemon">Volver</a>
-      </div>
-    </div>
+        </Box>
+      </CardContent>
+      <Link underline="none" href="?#/pokemon" color="grey.900">
+        Volver
+      </Link>
+    </Card>
   );
 };
